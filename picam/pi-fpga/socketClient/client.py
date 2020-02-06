151432ccb9d66a9263150757d245cc66507bdae3
Meng Wang <wzfftxwd@gmail.com>
2020-02-05 23:32:58 -0800

    server code modified

--- picam/pi-fpga/socketClient/client.py
+++ picam/pi-fpga/socketClient/client.py
@@ -1,25 +1,12 @@
 import socket
 import time
-from picamera import PiCamera
 import numpy as np
 
 print(socket.gethostname())
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host = socket.gethostname()
+host = socket.gethostbyname("10.2.0.15")
+
 print(host)
 
 port = 8080
-
-s.connect((host, port))
-
-camera = Picamera()
-camera.resolution = (640,480)
-time.sleep(2)
-img = np.empty((480, 640, 3), dtype=np.uint8)
-
-while True:
-    camera.capture(img, "rgb")
-    bytearray = img.tobytes()
-    s.send(bytearray)
-    time.sleep(1)
--- picam/pi-fpga/socketServer/server.py
+++ picam/pi-fpga/socketServer/server.py
@@ -1,28 +1,27 @@
 import socket
 
-if __name__ == '__main__':
-    port = 8080
-    s = socket.socket()
-    host = socket.gethostname()
-    s.bind((host, port))
-    s.listen(5)
+port = 1234
+host = '192.168.137.199'
 
-    imageFolder = "./images/"
-    imageFormat = ".png"
-    imageIndex = 0
+s = socket.socket()
+s.connect((host,port))
 
-    print('Server listening....')
-    conn, addr = s.accept()
-    print('Got connection from', addr)
+imageFolder = "./images/"
+imageFormat = ".png"
+imageIndex = 0
 
-    while True:
-        filename = imageFolder + str(imageIndex) + imageFormat
-        imageIndex += 1
+#print('Server listening....')
+#conn, addr = s.accept()
+#print('Got connection from', addr)
 
-        imagebytes = conn.recv(307200)
-        myfile = open(filename, 'wb')
-        myfile.write(imagebytes)
-        myfile.close()
+while True:
+    filename = imageFolder + str(imageIndex) + imageFormat
+    imageIndex += 1
 
-    print('Done receiving')
-    conn.close()
+    imagebytes = s.recv(307200)
+    myfile = open(filename, 'wb')
+    myfile.write(imagebytes)
+    myfile.close()
+
+print('Done receiving')
+conn.close()
