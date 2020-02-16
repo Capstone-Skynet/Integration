6a73382f9226309eb490610a9a492cbddb147799
Muchen He <m.he@alumni.ubc.ca>
2020-02-15 18:57:03 -0800

    Added argparsing and exception handling

--- SkynetWebApp/camtest/client.py
+++ SkynetWebApp/camtest/client.py
@@ -1,43 +1,72 @@
-import socketio
+import argparse
 import base64
+import cv2
+import numpy as np
+import socketio
+import sys
 import time
+import threading
 
-client = socketio.Client()
-img_prefix = "Images/"
-png = ".jpg"
-data = 1;
+# SETUP: Argument Setup
+arg_parser = argparse.ArgumentParser(description="Camera test program to connect to Skynet Webapp via socket.io")
+arg_parser.add_argument('--reconnect', help='Keep trying to reconnect to socket server even when connection refuses', action='store_true')
+arg_parser.add_argument('--reconnect-freq', help='The frequency to attempt reconnecting to socket IO server', default=3)
+args = arg_parser.parse_args()
 
-connection = 'http://localhost:80'
+# SETUP: Socket IO connections
+CONNECTION_URL = 'http://localhost:80'
 
-client.connect(connection)
-print ('my sid is ', client.sid)
-client.emit('pytest', 'pytest sent')
+def setup_socket():
+    client = socketio.Client()
+    while not client.connected:
+        try:
+            client.connect(CONNECTION_URL)
+        except socketio.exceptions.ConnectionError as e:
+            if args.reconnect:
+                print(f'Connection to {CONNECTION_URL} refused; attempting to reconnect (Control+C to quit).')
+                time.sleep(1 / args.reconnect_freq)
+                pass
+            else:
+                print(f'Connection to {CONNECTION_URL} refused.')
+                print(e)
+                sys.exit(1)
 
-@client.event
-def connect():
-    print('Connected to ', connection)
+    return client
 
-@client.event
-def connect_error():
-    print('Error, could not connect to ', connection)
+# @client.event
+# def connect():
+#     print('Connected to ', CONNECTION_URL)
 
-@client.event
-def disconnect():
-    print('Disconnected from ', connection)
+# @client.event
+# def connect_error():
+#     print('Error, could not connect to ', CONNECTION_URL)
 
-while 1:
-    filepath = img_prefix + str(data) + png
-    with open(filepath, 'rb') as imageFile:
-        image_data = base64.b64encode(imageFile.read())
-        img_string = str(image_data)
-        length = len(img_string)
-        client.emit('py_img_receive', img_string[2:length-1] )
-    time.sleep(0.4)
-    #print('Image sent') 
-    if (data == 10):
-        data = 1
-    else:
-        data += 1
-    
+# @client.event
+# def disconnect():
+#     print('Disconnected from ', CONNECTION_URL)
 
+# print ('my sid is ', client.sid)
+# client.emit('pytest', 'pytest sent')
 
+def main():
+    client = setup_socket()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Program Terminated')
+
+# while 1:
+#     filepath = img_prefix + str(data) + png
+#     with open(filepath, 'rb') as imageFile:
+#         image_data = base64.b64encode(imageFile.read())
+#         img_string = str(image_data)
+#         length = len(img_string)
+#         client.emit('py_img_receive', img_string[2:length-1] )
+#     time.sleep(0.4)
+#     #print('Image sent') 
+#     if (data == 10):
+#         data = 1
+#     else:
+#         data += 1
