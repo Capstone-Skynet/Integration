cc2765ec8aff02a4f77e902957bad8835f5155fd
Muchen He <m.he@alumni.ubc.ca>
2020-02-15 19:17:22 -0800

    Added opencv

--- SkynetWebApp/camtest/client.py
+++ SkynetWebApp/camtest/client.py
@@ -18,14 +18,14 @@ CONNECTION_URL = 'http://localhost:80'
 
 def setup_socket():
     client = socketio.Client()
-    while not client.connected:
+
+    while client.connected:
         try:
             client.connect(CONNECTION_URL)
         except socketio.exceptions.ConnectionError as e:
             if args.reconnect:
                 print(f'Connection to {CONNECTION_URL} refused; attempting to reconnect (Control+C to quit).')
                 time.sleep(1 / args.reconnect_freq)
-                pass
             else:
                 print(f'Connection to {CONNECTION_URL} refused.')
                 print(e)
@@ -51,11 +51,26 @@ def setup_socket():
 def main():
     client = setup_socket()
 
+    cap = cv2.VideoCapture(0)
+
+    while(True):
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+
+        # Display the resulting frame
+        cv2.imshow('frame',frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
+
 if __name__ == '__main__':
     try:
         main()
     except KeyboardInterrupt:
-        print('Program Terminated')
+        print('Program Terminated.')
 
 # while 1:
 #     filepath = img_prefix + str(data) + png
@@ -67,6 +82,6 @@ if __name__ == '__main__':
 #     time.sleep(0.4)
 #     #print('Image sent') 
 #     if (data == 10):
-#         data = 1
+#         data = 1 
 #     else:
 #         data += 1
