c27dc403361acb06120921def24dfa3d74cb81a2
Muchen He <m.he@alumni.ubc.ca>
2020-02-16 23:01:21 -0800

    Tidying up

--- SkynetWebApp/server.js
+++ SkynetWebApp/server.js
@@ -1,22 +1,25 @@
-let app = require('express')();
-let server = require('http').Server(app);
-let io = require('socket.io')(server);
+// Use Express as app, then set resources directory (static files)
+let express = require('express');
+let app = express();
+app.use(express.static(__dirname + 'public'))
 
-// var fs = require('fs');
-
-const LOCALHOST_PORT = 80;
-
-server.listen(LOCALHOST_PORT, () => {
-  console.log('Listening on port: ' + LOCALHOST_PORT);
-})
-// WARNING: app.listen(80) will NOT work here!
-
+// Set app to map root to index.html
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/public/index.html');
 });
 
+// Http server and socket io
+let server = require('http').Server(app);
+let io = require('socket.io')(server);
+
+// Http server set up connection
+const LOCALHOST_PORT = 80;
+server.listen(LOCALHOST_PORT, () => {
+  console.log('Listening on port: ' + LOCALHOST_PORT);
+})
+
+// Set up connections when socket connects
 io.on('connection', (socket) => {
-  // Set up connections when socket connects
 
   // Receiving data from Python program
   socket.on('PY_IMG_DATA', (imageData) => {
