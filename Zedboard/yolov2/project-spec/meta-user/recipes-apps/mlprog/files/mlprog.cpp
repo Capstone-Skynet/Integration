e3a55104229a9fbd5db0611cb061627313574323
Peter Deutsch <peterdeutsch@ece.ubc.ca>
2020-03-18 20:01:48 +0000

    Continued work on mlprog

--- Zedboard/yolov2/project-spec/meta-user/recipes-apps/mlprog/files/mlprog.cpp
+++ Zedboard/yolov2/project-spec/meta-user/recipes-apps/mlprog/files/mlprog.cpp
@@ -6,17 +6,74 @@
 #include <string.h>
 #include <time.h>
 #include "yolov2.h"
+#include <sys/socket.h> 
+#include <arpa/inet.h> 
+#include <unistd.h>
+#define PORT 8080 
+
+void fetchImage(int socket, image img)
+{
+    int bytesRead = 0;
+    int result;
+
+    int imageLength = 2076672;
+
+    bytesRead = 0;
+    while (bytesRead < imageLength) {
+        printf("%d bytes fetched!\n", bytesRead);
+        result = read(socket, ((char *) img.data) + bytesRead, imageLength - bytesRead);
+
+        if(result < 1) {
+            printf("ERROR %d!\n", errno);
+            exit(-1);
+        }
+
+        bytesRead += result;
+    }
+
+}
+    
 
 int main( int argc, char *argv[])
 {
-	unsigned int WEIGHT_BASE = 0x10000000;
+   
+    int sock = 0, valread; 
+    struct sockaddr_in serv_addr; 
+    char *hello = "Hello from client"; 
+    char buffer[1024] = {0}; 
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
+    { 
+        printf("\n Socket creation error \n"); 
+        return -1; 
+    } 
+
+    serv_addr.sin_family = AF_INET; 
+    serv_addr.sin_port = htons(PORT); 
+       
+    // Convert IPv4 and IPv6 addresses from text to binary form 
+    if(inet_pton(AF_INET, "192.168.1.11", &serv_addr.sin_addr)<=0)  
+    { 
+        printf("\nInvalid address/ Address not supported \n"); 
+        return -1; 
+    } 
+
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
+    { 
+        printf("\nConnection Failed \n"); 
+        return -1; 
+    } 
+    
+    image sized = make_empty_image(416,416,3);
+    sized.data = (float *)calloc(416*416*3, sizeof(float));
+
+    unsigned int WEIGHT_BASE = 0x10000000;
     unsigned int BETA_BASE = 0x1C25F000;
     unsigned int MEM_BASE  = 0x1C26A000;// 49770*1024*4 = 203857920 = C26A000
 
     printf("YOLOv2 TEST Begin\n");
     char **names = get_labels("coco.names");
 	int x;
-	for(x=0;x<80;x++)//80 classe labels
+	for(x=0;x<80;x++)//80 class labels
 	{
 		printf("[%d]%s\n",x,names[x]);
 	}
@@ -24,59 +81,41 @@ int main( int argc, char *argv[])
     network *net = load_network("yolov2.cfg", "yolov2.weights", 0);
     set_batch_network(net, 1);
 
-////////////////////load img resize img begin
-    char buff[256];
-    char *input_imgfn = buff;
-    if(argc==1)
-    {
-    	strncpy(input_imgfn, "person.jpg", 256);
+    fetchImage(sock, sized);
+
+    double time;
+    layer l = net->layers[net->n-1];
+    float *X = sized.data;
+
+    int i;
+    float nms=.2;
+    float thresh = .6;
+    float hier_thresh = .6;
+    int nboxes = 0;
+
+    detection *dets;
+
+    for (i = 0; i < 1; i++) {
+        time = what_time_is_it_now();
+        yolov2_hls_ps(net, X,WEIGHT_BASE,BETA_BASE,MEM_BASE);
+        printf("Predicted in %f seconds.!\n",what_time_is_it_now()-time);
+
+        dets = get_network_boxes(net, sized.w, sized.h, thresh, hier_thresh, 0, 1, &nboxes);
+        printf("%d\n", nboxes);
+        if (nms) do_nms_sort(dets, nboxes, l.classes, nms);
     }
-    else
-    {
-    	strncpy(input_imgfn, argv[1], 256);
-    }
-	printf("Input img:%s\n",input_imgfn);
-	image im = load_image_stb(input_imgfn, 3);//3 channel img
-	printf("img w=%d,h=%d,c=%d\n",im.w,im.h,im.c);
-	image sized = letterbox_image(im, 416, 416);
-	save_image_png(sized, "sized");// convert to yolov3 net input size 416x416x3
-////////////////////load img resize img end
-
-//	time_t first, second;
-	double time;
-	layer l = net->layers[net->n-1];
-        float *X = sized.data;
-
-        int i;
-        float nms=.2;
-        float thresh = .6;
-        float hier_thresh = .6;
-        int nboxes = 0;
-
-        detection *dets;
-
-        for (i = 0; i < 1; i++) {
-            time = what_time_is_it_now();
-            yolov2_hls_ps(net, X,WEIGHT_BASE,BETA_BASE,MEM_BASE);
-            printf("Predicted in %f seconds.!\n",what_time_is_it_now()-time);
-
-            dets = get_network_boxes(net, im.w, im.h, thresh, hier_thresh, 0, 1, &nboxes);
-            printf("%d\n", nboxes);
-            if (nms) do_nms_sort(dets, nboxes, l.classes, nms);
-        }
 
     printf("DONE3!");
-    draw_detections(im, dets, nboxes, thresh, names, alphabet, l.classes);
+    draw_detections(sized, dets, nboxes, thresh, names, alphabet, l.classes);
 
     free_detections(dets, nboxes);
 	
 ///////////////////write predictions img
-	save_image_png(im, "predictions");// output
-
-	free_image(im);
+    save_image_png(sized, "predictions");// output
+    
     free_image(sized);
 
-	printf("YOLOv2 TEST1 End\n");
+    printf("YOLOv2 TEST1 End\n");
 
     return 0;
 }
