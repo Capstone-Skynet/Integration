46bed04bd640ef9b0893eebc7b75aff2efb8e4d7
Arthur Hsueh <ah11962@outlook.com>
2020-03-28 18:25:38 -0700

    Arthur/wilson combine adjustments

--- SkynetWebApp/server.js
+++ SkynetWebApp/server.js
@@ -19,20 +19,18 @@ io.on('connection', (socket) => {
   // Receiving ml result from Python program
   socket.on('PY_ML_RESULT', (result) => {
     console.log(result);
+
+  // Changing settings from webapp
+  //gs_threshold, gs_mode
   socket.on('WEB2SRV_threshold_val', (value) => {
     console.log("Threshold Value Updated to " + value)
-    socket.broadcast.emit('SRV2RPI_threshold_val', value);
+    socket.broadcast.emit('gs_threshold', value);
   });
 
   socket.on('WEB2SRV_detect_obj_on', (value) => {
     console.log(value + " detection turned on");
-    socket.broadcast.emit('SRV2RPI_detect_obj_on', value);
+    socket.broadcast.emit('gs_mode', value);
   });
 
-  socket.on('WEB2SRV_detect_obj_off', (value) => {
-    console.log(value + " detection turned off");
-    socket.broadcast.emit('SRV2RPI_detect_obj_off', value);
-
-  });
 });
 
