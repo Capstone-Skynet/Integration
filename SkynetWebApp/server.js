0303284079baf4e411c7a4b1d25bb3b0fc547dc0
Arthur Hsueh <ah11962@outlook.com>
2020-03-28 18:19:02 -0700

    threshold and class detection

--- SkynetWebApp/server.js
+++ SkynetWebApp/server.js
@@ -15,5 +15,20 @@ io.on('connection', (socket) => {
   socket.on('PY_IMG_DATA', (imageData) => {
     socket.broadcast.emit('SRV_IMG_DATA', imageData);
   });
+
+  socket.on('WEB2SRV_threshold_val', (value) => {
+    console.log("Threshold Value Updated to " + value)
+    socket.broadcast.emit('SRV2RPI_threshold_val', value);
+  });
+
+  socket.on('WEB2SRV_detect_obj_on', (value) => {
+    console.log(value + " detection turned on");
+    socket.broadcast.emit('SRV2RPI_detect_obj_on', value);
+  });
+
+  socket.on('WEB2SRV_detect_obj_off', (value) => {
+    console.log(value + " detection turned off");
+    socket.broadcast.emit('SRV2RPI_detect_obj_off', value);
+  });
 });
 
